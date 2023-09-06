6d76eaf0a82e3d7d2ba414c4c7aec1528ac8a7da
crc8109 <crc8109@pm.me>
2023-09-05 17:04:11 -0700

    Create template/skeleton for script

--- defense_calculator.py
+++ defense_calculator.py
@@ -1,5 +1,6 @@
 def defense_calculator(opponent_type1, opponent_type2):
     """This function will calculate the offensive multiplier for opponent Pokemon's type(s)"""
+    
     # Dictionary for the defensive multipliers for each of the types:
     defense_multiplier_dict = {
         "normal": {
@@ -27,4 +28,87 @@ def defense_calculator(opponent_type1, opponent_type2):
             1: ["normal", "fighting", "psychic", "rock", "ghost", "dragon", "dark", "steel", "fairy"],
             0.5: ["water", "electric", "grass", "ground"]
         },
+        "ice": {
+            2: ["fire", "fighting", "rock", "steel"],
+            1: ["normal", "water", "electric", "grass", "poison", "ground", "flying", "psychic", "bug", "ghost", "dragon", "dark", "fairy"],
+            0.5: ["ice"]
+        },
+        "fighting": {
+            2: ["flying", "psychic", "fairy"],
+            1: ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground", "ghost", "dragon", "steel"],
+            0.5: ["bug", "rock", "dark"]
+        },
+        "poison": {
+            2: ["ground", "psychic"],
+            1: ["normal", "fire", "water", "electric", "ice", "flying", "rock", "ghost", "dragon", "dark", "steel"],
+            0.5: ["grass", "fighting", "poison", "bug", "fairy"]
+        },
+        "ground": {
+            2: ["water", "grass", "ice"],
+            1: ["normal", "fire", "fighting", "ground", "flying", "psychic", "bug", "ghost", "dragon", "dark", "steel", "fairy"],
+            0.5: ["poison", "rock"],
+            0: ["electric"]
+        },
+        "flying": {
+            2: ["electric", "ice", "rock"],
+            1: ["normal", "fire", "water", "poison", "flying", "psychic", "ghost", "dragon", "dark", "steel", "fairy"],
+            0.5: ["grass", "fighting", "bug"],
+            0: ["ground"]
+        },
+        "psychic": {
+            2: ["bug", "ghost", "dark"],
+            1: ["normal", "fire", "water", "electric", "grass", "ice", "poison", "ground", "flying", "rock", "dragon", "steel", "fairy"],
+            0.5: ["fighting", "psychic"]
+        },
+        "bug": {
+            2: ["fire", "flying", "rock"],
+            1: ["normal", "water", "electric", "ice", "poison", "psychic", "bug", "ghost", "dragon", "dark", "steel", "fairy"],
+            0.5: ["grass", "fighting", "ground"]
+        },
+        "rock": {
+            2: ["water", "grass", "fighting", "ground", "steel"],
+            1: ["electric", "ice", "psychic", "bug", "rock", "ghost", "dragon", "dark", "fairy"],
+            0.5: ["normal", "fire", "poison", "flying"]
+        },
+        "ghost": {
+            2: ["ghost", "dark"],
+            1: ["fire", "water", "electric", "grass", "ice", "ground", "flying", "psychic", "rock", "dragon", "steel", "fairy"],
+            0.5: ["poison", "bug"],
+            0: ["normal", "fighting"]
+        },
+        "dragon": {
+            2: ["ice", "dragon", "fairy"],
+            1: ["normal", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "ghost", "dark", "steel"],
+            0.5: ["fire", "water", "electric", "grass"]
+        },
+        "dark": {
+            2: ["fighting", "bug", "fairy"],
+            1: ["normal", "fire", "water", "electric", "grass", "ice", "poison", "ground", "flying", "rock", "dragon", "steel"],
+            0.5: ["ghost", "dark"],
+            0: ["psychic"]
+        },
+        "steel": {
+            2: ["fire", "fighting", "ground"],
+            1: ["water", "electric", "ghost", "dark"],
+            0.5: ["normal", "grass", "ice", "flying", "psychic", "bug", "rock", "dragon", "steel", "fairy"],
+            0: ["poison"]
+        },
+        "fairy": {
+            2: ["poison", "steel"],
+            1: ["normal", "fire", "water", "electric", "grass", "ice", "ground", "flying", "psychic", "rock", "ghost", "fairy"],
+            0.5: ["fighting", "bug", "dark"],
+            0: ["dragon"]
+        }
     }
+    defense_analysis = {}
+            
+    defense_analysis[opponent_type1, opponent_type2] = defense_multiplier_dict[opponent_type1]
+
+    # for multiplier in defense_analysis[opponent_type2]:
+        
+        
+
+    # print("\nHere's the defensive analysis...")
+    # print(defense_analysis)
+   
+    # print("that's the analysis!")
